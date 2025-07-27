65fc22e13ab8a7ee381cb9e47b7b288c9391f3a7
Zongqian Li <127303250+ZongqianLi@users.noreply.github.com>
2025-07-27 17:41:40 +0800

    Add files via upload

---
+++ lcb_runner/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lcb_runner",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # 在这里列出你的依赖包
+        # 例如: "numpy", "pandas", 等
+    ],
+)
